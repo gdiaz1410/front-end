96879151c722f0b82484d5d976ed1df38478b75d
Gabriel Diaz <gdiaz1410@gmail.com>
2016-12-23 22:37:43 -0600

    scroll effect complete

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-
+  // sticky nav
   $('.js-section-features').waypoint(function(direction) {
     if (direction === "down") {
       $('nav').addClass('sticky');
@@ -10,15 +10,34 @@ $(document).ready(function() {
   }, {
     offset: '80px'
   });
-  // offset 60 px before section
+  // offset 80px before section
 
 
-  // var waypoints = $('#handler-first').waypoint(function(direction) {
-  //   notify(this.element.id + ' hit 25% from top of window')
-  // }, {
-  //   offset: '25%'
-  // }
+  // scroll effect on button click
+  $('.js-scroll-to-plan').click(function() {
+    $('html, body').animate({scrollTop: $('.js-plans').offset().top}, 1000);
+  });
 
+  $('.js-scroll-to-about').click(function() {
+    $('html, body').animate({scrollTop: $('.js-section-features').offset().top}, 1000);
+  });
+
+
+  // navbar scroll
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
 
 
